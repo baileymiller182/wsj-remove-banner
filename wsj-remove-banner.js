b74fd5a908231c8229f7d28d8e3669127074f124
Bailey Miller <baileymiller182@gmail.com>
2020-10-22 11:53:58 -0700

    Delay until DOM is hopefully loaded up

--- wsj-remove-banner.js
+++ wsj-remove-banner.js
@@ -1,1 +1,6 @@
-document.getElementById("wrapper-AD_PUSH").remove();
+var delayTimeMS = 2000;
+
+setTimeout(function() {
+   document.getElementById("wrapper-AD_PUSH").remove();
+   console.log('Be gone foul advertisement.');
+}, delayTimeMS);
